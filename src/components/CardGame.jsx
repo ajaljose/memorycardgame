3f5020cd480ba88190d008b78ab7461102266672
Ajal Jose <ajaljosem@gmail.com>
2025-01-17 13:21:16 +0530

    completed

--- src/components/CardGame.jsx
+++ src/components/CardGame.jsx
@@ -1,31 +1,50 @@
 "use client";
-import GameStateHook from '@/hooks/GameStateHook';
-import React, { useEffect, useState } from 'react'
+import { useEffect } from "react";
+import GameStateHook from "@/hooks/GameStateHook";
 const CardGame = () => {
-    const { boardCards, shuffle } = GameStateHook();
+  const {
+    boardCards,
+    flippedCards,
+    matchedCards,
+    shuffle,
+    handleCardClick,
+    moves,
+    resetGame
+  } = GameStateHook();
 
-    useEffect(() => {
-        shuffle();
-      }, []);
+  useEffect(() => {
+    shuffle();
+  }, []);
+
   return (
     <div className="game-container w-full">
+      <h1>No. of Moves: {moves}</h1>
       <div className="cardGrid mt-20">
-
         {boardCards.map((data, index) => {
+          const flipped = flippedCards.includes(index) ? true : false;
+          const matched = matchedCards.includes(index) ? true : false;
           return (
             <div
-            key={index}
-              className={`card ${false ? "active" : ""} `}
+              key={index}
+              onClick={() => handleCardClick(index, data)}
+              className={`card ${flipped || matched ? "active" : ""} ${
+                matched ? "matched" : ""
+              }`}
             >
               <div className="card-front">{data}</div>
               <div className="card-back">?</div>
             </div>
           );
         })}
-
       </div>
+      <button className="mt-5 mb-5 pl-5 pr-5" onClick={()=>resetGame()}>Restart Game</button>
+{
+    matchedCards.length==16?
+
+      <h1>YOU WON the Game</h1>:""
+}
     </div>
   );
-}
+};
 
 export default CardGame;
--- src/hooks/GameStateHook.js
+++ src/hooks/GameStateHook.js
@@ -3,7 +3,9 @@ import { useState } from 'react'
 const GameStateHook = () => {
 
     const [boardCards, setBoardCards] = useState([]);
-  
+    const [flippedCards, setFlippedCards] = useState([]);
+    const [matchedCards, setMatchedCards] = useState([]);
+    const [moves, setMoves] = useState(0);
     const cardData = [
       'A',
       'B',
@@ -22,9 +24,37 @@ const GameStateHook = () => {
   
       setBoardCards(shuffledCards);
     };
-
   
-    return { boardCards, shuffle };
+    const handleCardClick = (index) => {
+      if (!flippedCards.includes(index)) {
+
+        if (flippedCards.length === 1) {
+          const firstIdx = flippedCards[0];
+          const secondIdx = index;
+          if (boardCards[firstIdx] === boardCards[secondIdx]) {
+            setMatchedCards((prev) => [...prev, firstIdx, secondIdx]);
+          }else{
+            setTimeout(() => {
+              setFlippedCards([]);
+            }, 1000);
+          }
+          
+          setFlippedCards([...flippedCards, index]);
+        } else if (flippedCards.length === 2) {
+          setFlippedCards([index]);
+        } else {
+          setFlippedCards([...flippedCards, index]);
+        }
+        setMoves(moves+1);
+      }
+    };
+  const resetGame=()=>{
+    shuffle();
+    setFlippedCards([]);
+    setMatchedCards([]);
+    setMoves(0);
+  }
+    return { boardCards, flippedCards, matchedCards, shuffle, handleCardClick,moves,resetGame };
 
 }
 
